1cb4500672ed3c9b3b7dcf6203662b75adc55b1b
Dianu <dmotturamoreno@uade.edu.ar>
2025-06-13 16:51:31 -0300

    Modo oscuro implementado

--- js/script.js
+++ js/script.js
@@ -1,27 +1,16 @@
-// Validación y obtención de datos para formulario de contacto
-const form = document.getElementById("form-contacto");
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const nombre = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const mensaje = document.getElementById("mensaje").value;
-
-    if (nombre.trim() === "" || email.trim() === "" || mensaje.trim() === "") {
-        Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Todos los campos son obligatorios",
-        });
-        return;
-    }
-
-    Swal.fire({
-        icon: "success",
-        title: `Gracias por tu contacto, ${nombre}!`,
-        text: "En breve te estaré respondiendo.",
-    });
-
-    form.reset();
+const toggle = document.querySelector('.toggle-switch input[type="checkbox"]');
+toggle.addEventListener("change", function () {
+  document.body.classList.toggle("dark", this.checked);
 });
+
+// Guardar preferencia
+toggle.addEventListener("change", function () {
+  document.body.classList.toggle("dark", this.checked);
+  localStorage.setItem("theme", this.checked ? "dark" : "light");
+});
+
+// Al cargar la página, aplicar preferencia
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+  toggle.checked = true;
+}
